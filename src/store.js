91ffd43efd588362974820616c6373da7ef89300
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-06-26 01:25:59 +0300

    testing multiple tasks

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const store = ({set}) => ({
-    tasks: [{title: 'TaskTest', state: 'PLANNED'}]
+    tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}]
 });
 
 export const useStore = create(store);
