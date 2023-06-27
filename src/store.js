889ce9930d41e7a5454e779e709f67143b4bc7fb
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-06-27 21:14:17 +0300

    another task

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const store = ({set}) => ({
-    tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}, {title: 'TaskTest4', state: 'PLANNED'}]
+    tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}, {title: 'TaskTest4', state: 'PLANNED'}, {title: 'TaskTest5', state: 'PLANNED'}]
 });
 
 export const useStore = create(store);
