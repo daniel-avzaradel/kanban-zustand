db47a585c2dcb5bf457bdf8e184780592dae6445
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-06-28 23:46:31 +0300

    created delete function

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,13 +6,14 @@ const STATUS = 'PLANNED'
 
 export default function Task({title}) {
 
-    const task = useStore(store => store.tasks.find(task => task.title === title))
+    const task = useStore(store => store.tasks.find(task => task.title === title));
+    const deleteTask = useStore(store => store.deleteTask)
 
     return (
         <div className="task">
             <div>{task.title}</div>
             <div className='bottomWrapper'>
-                <div></div>
+                <div><button onClick={() => deleteTask(title)}>Delete</button></div>
                 <div className={classNames('status', task.state)}>{task.state}</div>
             </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const store = (set) => ({
     tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}],
     addTask: (title, state) => set((store) => ({tasks: [...store.tasks, { title, state } ]})),
-    deleteTask: (title) => set((store) => {tasks: store.tasks.filter(task => task.title != title)})
+    deleteTask: (title) => set((store) => ({tasks: store.tasks.filter(task => task.title !== title)}))
 });
 
 export const useStore = create(store);
