eb7bde40ef4d7767fdb5b8727397ffd2473ef03a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-06-28 23:42:58 +0300

    autofocus and kreypress to the modal

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,22 +1,50 @@
-import './Column.css';
-import Task from './Task';
-import { useStore } from '../store.js';
+import "./Column.css";
+import Task from "./Task";
+import { useStore } from "../store.js";
+import { useState } from "react";
 
+const Column = ({ state }) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-const Column = ({state}) => {
+  const tasks = useStore((store) =>
+    store.tasks.filter((task) => task.state === state)
+  );
+  const addTask = useStore((store) => store.addTask);
 
-    const tasks = useStore((store) => store.tasks.filter((task) => task.state === state));
-    const addTask = useStore((store) => store.addTask)
+  const submit = () => {
+    addTask(text, state);
+    setText("");
+    setOpen(false);
+  };
 
-    return (
-        <div className='column'>
-            <div className='titleWrapper'>
-                <p>{state}</p>
-                <button onClick={() => addTask('asdasdas', state)}>Add</button>
-            </div>
-            {tasks.map((task, i) => <Task title={task.title} key={task.title + i} />)}
+const keypress = (e) => e.key === 'Enter' && submit()
+
+  return (
+    <div className="column">
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {tasks.map((task, i) => (
+        <Task title={task.title} key={task.title + i} />
+      ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input
+              type="text"
+              autoFocus
+              onChange={(e) => setText(e.target.value)}
+              onKeyDown={(e) => keypress(e)}
+              value={text}
+            />
+            <button onClick={submit}>Submit</button>
+          </div>
         </div>
-    )
-}
+      )}{" "}
+    </div>
+  );
+};
 
-export default Column
+export default Column;
--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { create } from "zustand";
 
 const store = (set) => ({
     tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}],
-    addTask: (title, state) => set((store) => ({tasks: [...store.tasks, { title, state } ]}))
+    addTask: (title, state) => set((store) => ({tasks: [...store.tasks, { title, state } ]})),
+    deleteTask: (title) => set((store) => {tasks: store.tasks.filter(task => task.title != title)})
 });
 
 export const useStore = create(store);
