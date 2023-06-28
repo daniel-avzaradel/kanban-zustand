461427d1a2b8a6dd59c1c947dcc82f577436bb93
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-06-28 22:57:41 +0300

    adding tasks via add button

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -6,10 +6,14 @@ import { useStore } from '../store.js';
 const Column = ({state}) => {
 
     const tasks = useStore((store) => store.tasks.filter((task) => task.state === state));
-    console.log(tasks);
+    const addTask = useStore((store) => store.addTask)
+
     return (
         <div className='column'>
-            <p>{state}</p>
+            <div className='titleWrapper'>
+                <p>{state}</p>
+                <button onClick={() => addTask('asdasdas', state)}>Add</button>
+            </div>
             {tasks.map((task, i) => <Task title={task.title} key={task.title + i} />)}
         </div>
     )
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 
-const store = ({set}) => ({
-    tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}, {title: 'TaskTest4', state: 'PLANNED'}, {title: 'TaskTest5', state: 'PLANNED'}]
+const store = (set) => ({
+    tasks: [{title: 'TaskTest', state: 'PLANNED'}, {title: 'TaskTest2', state: 'ONGOING'}, {title: 'TaskTest3', state: 'PLANNED'}],
+    addTask: (title, state) => set((store) => ({tasks: [...store.tasks, { title, state } ]}))
 });
 
 export const useStore = create(store);
