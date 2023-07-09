2277569cae4c695bb43acf4e9b14cd0ed21afa4a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-07-10 00:00:29 +0300

    finished second test to add item to the tasks object

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -10,9 +10,22 @@ function TestComponent({selector, effect}) {
 }
 
 test("should return default value at the start", () => {
-    const selector = (store) => store.tasks;
+    const selector = (store) => (store.tasks);
     const effect = vi.fn();
 
     render(<TestComponent selector={selector} effect={effect} />)
     expect(effect).toHaveBeenCalledWith([])
-})
+});
+
+test("should add an item to the store and return the effect", () => {
+    const selector = (store) => ({tasks: store.tasks, addTask: store.addTask});
+    const effect = vi.fn().mockImplementation((items) => {
+        if(items.tasks.length === 0) {
+            items.addTask('a', 'b')
+        }
+    });
+
+    render(<TestComponent selector={selector} effect={effect} />)
+    expect(effect).toHaveBeenCalledTimes(2)
+    expect(effect).toHaveBeenCalledWith(expect.objectContaining({tasks: [{title: 'a', state: 'b'}]}))
+});
